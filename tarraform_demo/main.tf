80cdadd474254797c309d8f6ad55af7c18b97a1c
Ibrahim <ibrahimnader399@gmail.com>
2024-01-30 05:33:37 +0200

    added the terraform part

---
+++ tarraform_demo/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.13.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = "./keys/my-creds.json"
+  project = var.project
+  region  = var.region
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.bucket
+  location      = var.location
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = "demo_dataset"
+}
---
+++ tarraform_demo/variables.tf
@@ -0,0 +1,20 @@
+variable "project" {
+  description = "Project"
+  default     = "terra-412514"
+}
+
+variable "region" {
+  description = "region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "location"
+  default     = "US"
+}
+
+
+variable "bucket" {
+  description = "My storage bucket name"
+  default     = "terraform-demo-bucket-412514"
+}
